d7eec478dd16d7b23533bbea67d1816a2cdfcf1f
byiringiroaimefils <aimefils173@example.com>
2024-12-02 17:11:33 +0200

    Add new feature: add file upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 require('dotenv').config()
 
 var app = express();
@@ -9,6 +11,14 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype, 
+    size: req.file.size,
+  });
 });
 
 
